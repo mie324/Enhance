c939efbcf6b38da0a8bb031be2320ed524261891
arifmoh2 <43102928+arifmoh2@users.noreply.github.com>
2018-11-18 15:18:27 -0500

    dataset

---
+++ dataset.py
@@ -0,0 +1,16 @@
+import torch.utils.data as data
+
+
+class ImgDataset(data.Dataset):
+
+    def __init__(self, features, labels):
+        self.features = features
+        self.labels = labels
+
+    def __len__(self):
+        return len(self.features)
+
+    def __getitem__(self, index):
+        features = self.features[index]
+        label = self.labels[index]
+        return features, label
