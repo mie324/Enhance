d1d7f7f5c056d55e3e619b409bcac488f53200ec
sjameel98 <saadjameel98@gmail.com>
2018-11-24 20:00:03 -0500

    evaluating PSNR function done

--- evaluating.py
+++ evaluating.py
@@ -1,10 +1,27 @@
 import matplotlib.pyplot as plt
 import torch
-import tensorflow as tf
-start_path = 'results/'
+import numpy as np
+from skimage.transform import resize
+from skimage.measure import compare_psnr
 
 
-start_path = 'results/'
-counter = 0
-for filename in os.listdir(start_path):
-    if filename.startswith('real'):
+def evaluate(lowres_images, generated_images, highres_images):
+    batch_size = highres_images.shape[0]
+    interpolated_images = np.zeros(highres_images.shape)
+
+    psnrint = np.zeros(batch_size)
+    psnrgen = np.zeros(batch_size)
+
+    #perform interpolation
+    for i in range(batch_size):
+        interpolated_images[i] = resize(lowres_images[i], (112, 92))
+
+    #calculate psnr of batch
+    for i in range(batch_size):
+        psnrint[i] = compare_psnr(highres_images[i], interpolated_images[i])
+        psnrgen[i] = compare_psnr(highres_images[i], generated_images[i])
+
+    total_psnrint = np.sum(psnrint)
+    total_psnrgen = np.sum(psnrgen)
+
+    return total_psnrgen, total_psnrint
